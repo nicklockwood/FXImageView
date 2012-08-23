fa494c161bfe793f9ae6c65558638c8a649f1f28
Rob Witman <rwitman@gmail.com>
2012-08-23 15:37:26 -0400

    Adjusted the Advanced Demo to utilize on device ALAssets for the images used.  This will illustrate an issue I am having where previous images are being used instead of the proper one.

--- Examples/Advanced Example/FXImageViewDemo/ViewController.h
+++ Examples/Advanced Example/FXImageViewDemo/ViewController.h
@@ -6,11 +6,18 @@
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
+#import <AssetsLibrary/AssetsLibrary.h>
 #import <UIKit/UIKit.h>
 #import "FXImageView.h"
 #import "iCarousel.h"
 
-@interface ViewController : UIViewController <iCarouselDataSource>
+@interface ViewController : UIViewController <iCarouselDataSource, iCarouselDelegate> {
+    // asset library
+    ALAssetsLibrary*                assetsLibrary;      ///< The asset library pointer used while accessing assets
+    NSMutableArray*                 groups;             ///< An array of [ALAssetsGroup] objects that hold all groups in a library
+    
+    IBOutlet UILabel*               groupLabel;         ///< The label indicating the common name of the selected group
+}
 
 @property (nonatomic, strong) IBOutlet iCarousel *carousel;
 
